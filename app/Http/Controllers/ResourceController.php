4b991d81e1efb28975b7c6b04892d20be73d90eb
yasin <yascanary0@gmail.com>
2023-06-02 21:08:57 +0330

    Resources list

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Resource;
 use Illuminate\Http\Request;
 
 class ResourceController extends Controller
 {
-    //
+    public function list()
+    {
+        return response()->json(Resource::paginate());
+    }
+
+    public function subscribe(Request $request, Resource $resource)
+    {
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\InstagramController;
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ResourceController;
 use App\Http\Controllers\TwitterController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,9 @@ Route::prefix("/instagram")->group(function() {
 Route::prefix("/twitter")->group(function() {
     Route::get("/addNew", [TwitterController::class, "addNew"]);
     Route::get("/search", [TwitterController::class, "search"]);
+});
+
+Route::prefix("/resource")->group(function() {
+    Route::get("/list", [ResourceController::class, "list"]);
+    Route::get("/{resource}/subscribe", [ResourceController::class, "subscribe"]);
 });
