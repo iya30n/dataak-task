899e0fd2c728b24e00e9d03a130f895bc6341e0e
yasin <yascanary0@gmail.com>
2023-06-02 17:58:43 +0330

    search for instgram and twitter

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchRequest;
+use App\Models\Twitter;
 use Illuminate\Http\Request;
 
 class TwitterController extends Controller
 {
-    //
+    public function addNew()
+    {
+        // create new record
+        // attach it to the elastic
+        // return response with done message and created record.
+    }
+
+    public function search(SearchRequest $request)
+    {
+        $searchQueries = [];
+
+        if ($request->has("start_date")) {
+            $endDate = $request->input("end_date", now()->format("Y-m-d"));
+            $searchQueries[] = sprintf("created_at:[%s TO %s]", $request->start_date, $endDate);
+        }
+
+        if ($request->has("title")) {
+            $searchQueries[] = sprintf('title:"%s"', $request->title);
+        }
+
+        if ($request->has("resource")) {
+            $searchQueries[] = sprintf('resource:"%s"', $request->resource);
+        }
+
+        if ($request->has("user")) {
+            $searchQueries[] = sprintf('(user_name:"%s" OR user_username:%s)', $request->user, $request->user);
+        }
+
+        $query = implode(" AND ", $searchQueries);
+
+        $result = Twitter::search($query)->paginate(15);
+
+        return $result->toJson();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,15 @@ use App\Http\Controllers\NewsController;
 use App\Http\Controllers\TwitterController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get("/test", fn() => "hey");
-
 Route::prefix("/news")->group(function() {
     Route::get("/addNew", [NewsController::class, "addNew"]);
     Route::get("/search", [NewsController::class, "search"]);
 });
 
-/* Route::prefix("/instagram")->group(function() {
+Route::prefix("/instagram")->group(function() {
     Route::get("/addNew", [InstagramController::class, "addNew"]);
 });
 
 Route::prefix("/twitter")->group(function() {
     Route::get("/addNew", [TwitterController::class, "addNew"]);
-}); */
+});
