727825a802c3c2410f1752169d87b3b2b52ec07a
yasin <yascanary0@gmail.com>
2023-06-02 18:04:29 +0330

    index id on instagram and twitter

--- database/migrations/2023_05_31_111949_create_instagrams_table.php
+++ database/migrations/2023_05_31_111949_create_instagrams_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('instagrams', function (Blueprint $table) {
-            $table->id();
+            $table->id()->index();
             $table->string('title');
 
             $table->string('resource');
--- database/migrations/2023_05_31_141903_create_twitters_table.php
+++ database/migrations/2023_05_31_141903_create_twitters_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('twitters', function (Blueprint $table) {
-            $table->id();
+            $table->id()->index();
             $table->text('content');
             $table->string("user_name");
             $table->string('resource');
