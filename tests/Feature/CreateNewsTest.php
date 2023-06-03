52d9e28c93ff393209157f08e70a8af868086e45
yasin <yascanary0@gmail.com>
2023-06-03 16:57:28 +0330

    testing create news

---
+++ tests/Feature/CreateNewsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Resource;
+use App\Mail\ResourceAlarmEmail;
+use Illuminate\Support\Facades\Mail;
+
+class CreateNewsTest extends TestCase
+{
+    public function test_create_news_and_send_email_to_subscribers()
+    {
+        Mail::fake();
+
+        $resourceId = 1;
+
+        $newsData = [
+            "title" => "test 1",
+            "resource_id" => $resourceId,
+            "user_name" => "mamd",
+            "content" => "this is the content of news one",
+            "link" => "https://something.com/something",
+            "agency_avatar" => "https://something.com/something",
+        ];
+
+        $response = $this->post('/api/news', $newsData);
+
+        $response->assertStatus(200);
+        $response->assertJson(["message" => "News created successfully."]);
+
+        $this->assertDatabaseHas("news", $newsData);
+
+        $users = Resource::find($resourceId)->subscribers;
+        Mail::assertQueued(ResourceAlarmEmail::class, function($mail) use($users) {
+            return $mail->hasTo($users->pluck('email'));
+        });
+    }
+}
